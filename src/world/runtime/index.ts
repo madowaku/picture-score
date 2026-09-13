428f44ff29b226f169f909eae372f11176c85f41
madowaku <67355875+madowaku@users.noreply.github.com>
2026-09-13 17:10:00 +0900

    feat: export score bloom runtime foundation

---
+++ src/world/runtime/index.ts
@@ -0,0 +1,9 @@
+export { EntityStore } from "./EntityStore";
+export { EventScheduler } from "./EventScheduler";
+export { LineageStore } from "./LineageStore";
+export type {
+  EntityOrigin,
+  ScoreBloomFrame,
+  WorldEntity,
+  WorldSnapshot,
+} from "./types";
