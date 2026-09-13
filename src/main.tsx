cfab226bff2983c21684f4910d0601492a2c9625
madowaku <67355875+madowaku@users.noreply.github.com>
2026-09-13 10:14:56 +0900

    feat: mount v0.8 release shell

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { LanguageProvider } from "./i18n/LanguageContext";
+import { ReleaseShell } from "./release/ReleaseShell";
 import "./styles.css";
 import "./wonder/wonder.css";
 import "./garden/alive.css";
 import "./garden/clearing.css";
+import "./release/release.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <LanguageProvider>
-      <App />
+      <ReleaseShell>
+        <App />
+      </ReleaseShell>
     </LanguageProvider>
   </React.StrictMode>,
 );
