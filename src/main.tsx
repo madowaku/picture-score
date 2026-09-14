e75c90ae17dfbf9d6557cd00ea62a8b52e2870b9
madowaku <67355875+madowaku@users.noreply.github.com>
2026-09-15 00:40:22 +0900

    feat: mount mobile studio chrome

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { LanguageProvider } from "./i18n/LanguageContext";
+import { MobileStudioChrome } from "./ui/MobileStudioChrome";
 import "./styles.css";
+import "./mobile-studio.css";
 import "./wonder/wonder.css";
 import "./garden/alive.css";
 import "./garden/clearing.css";
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <LanguageProvider>
       <App />
+      <MobileStudioChrome />
     </LanguageProvider>
   </React.StrictMode>,
 );
